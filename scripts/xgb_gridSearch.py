3e1dd82612af9541a3da418dd7dc6c9b2cfc5c74
Laurits <laurits.tani@gmail.com>
2019-11-13 22:01:41 +0200

    fix

--- scripts/xgb_gridSearch.py
+++ scripts/xgb_gridSearch.py
@@ -29,7 +29,7 @@ def main(param_file, nthread, sample_dir, outputDir):
     data_dict = mf.create_datasets(sample_dir, nthread)
     result_dict = gsm.perform_gridSearch(
         parameters,
-        GRID_SIZE
+        GRID_SIZE,
         nthread,
         xt.ensemble_fitnesses
     )
