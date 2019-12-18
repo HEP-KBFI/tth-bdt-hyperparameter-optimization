a223a07bd7e75400019822416c5b181a63a5bb51
Diana <diana.rand@outlook.com>
2019-12-18 10:44:35 +0200

    Typo fix

--- python/slurm_main.py
+++ python/slurm_main.py
@@ -130,7 +130,7 @@ def run_iteration(
         test dataset
     '''
     output_dir = os.path.expandvars(global_settings['output_dir'])
-    if sample_size = 0:
+    if sample_size == 0:
         opt_settings = universal.read_settings(global_settings['optimization_algo'])
         sample_size = opt_settings['sample_size']
     parameters_to_file(output_dir, parameter_dicts)
