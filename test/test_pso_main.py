15a5b6d25a318416484f80c746523a53b47542b1
Laurits <laurits.tani@gmail.com>
2019-12-18 16:45:40 +0200

    update tests

--- test/test_pso_main.py
+++ test/test_pso_main.py
@@ -450,7 +450,7 @@ def test_track_best_scores2():
         'best_test_auc': 0.6,
         'best_train_auc': 0.6,
         'best_avg_scores': [1, 2],
-        'best_compactnesses': [0.2, 0.3],
+        'compactnesses': [0.2, 0.3],
         'best_fitnesses': [0.8, 0.9]
     }
     fitnesses = [1, 0.5, 0.6]
@@ -486,16 +486,16 @@ def test_track_best_scores2():
         'best_test_auc': 1,
         'best_train_auc': 1,
         'best_avg_scores': [1, 2],
-        'best_compactnesses': [0.2, 0.3],
+        'compactnesses': [0.2, 0.3, 0.1],
         'best_fitnesses': [0.8, 0.9],
         'feature_importances': {'f1': 1, 'f2': 2},
         'best_parameters': {'foo': 1, 'bar': 2},
-        'best_fitness': 1,
-        'best_g_score': [1],
-        'best_f1_score': [1],
-        'best_d_score': [1],
-        'best_test_auc': [1],
-        'best_train_auc': [1]
+        'best_fitnesses': 1,
+        'best_g_scores': [1],
+        'best_f1_scores': [1],
+        'best_d_scores': [1],
+        'best_test_aucs': [1],
+        'best_train_aucs': [1]
     }
     assert result_dict1 == expected
 
