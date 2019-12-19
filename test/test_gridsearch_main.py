8ae1e511d9bcc456c88df61570a8248f04c47078
Laurits <laurits.tani@gmail.com>
2019-12-19 10:53:08 +0200

    update tests

--- test/test_gridsearch_main.py
+++ test/test_gridsearch_main.py
@@ -36,9 +36,9 @@ data_dict = mf.create_datasets(sample_dir, 16)
 def test_single_paramset():
     grid_size = 3
     parameters = [
-        {'p_name': 'first', 'range_start': 1, 'range_end': 3, 'true_int': 'True'},
-        {'p_name': 'second', 'range_start': 5, 'range_end': 7, 'true_int': 'False'},
-        {'p_name': 'third', 'range_start': 8, 'range_end': 10, 'true_int': 'True'}
+        {'p_name': 'first', 'range_start': 1, 'range_end': 3, 'true_int': 0},
+        {'p_name': 'second', 'range_start': 5, 'range_end': 7, 'true_int': 0},
+        {'p_name': 'third', 'range_start': 8, 'range_end': 10, 'true_int': 1}
     ]
     iterations = [0, 1, 0]
     expected = {
@@ -53,9 +53,9 @@ def test_single_paramset():
 def test_initialize_values():
     grid_size = 1
     parameters = [
-        {'p_name': 'first', 'range_start': 1, 'range_end': 3, 'true_int': 'True'},
-        {'p_name': 'second', 'range_start': 5, 'range_end': 7, 'true_int': 'False'},
-        {'p_name': 'third', 'range_start': 8, 'range_end': 10, 'true_int': 'True'}
+        {'p_name': 'first', 'range_start': 1, 'range_end': 3, 'true_int': 1},
+        {'p_name': 'second', 'range_start': 5, 'range_end': 7, 'true_int': 0},
+        {'p_name': 'third', 'range_start': 8, 'range_end': 10, 'true_int': 1}
     ]
     expected1 = [{
         'first': 2.,
@@ -69,8 +69,8 @@ def test_initialize_values():
 def test_initialize_values2():
     grid_size = 2
     parameters = [
-        {'p_name': 'first', 'range_start': 1, 'range_end': 2, 'true_int': 'True'},
-        {'p_name': 'second', 'range_start': 5, 'range_end': 6, 'true_int': 'False'},
+        {'p_name': 'first', 'range_start': 1, 'range_end': 2, 'true_int': 1},
+        {'p_name': 'second', 'range_start': 5, 'range_end': 6, 'true_int': 0},
     ]
     expected1 = {
         'first': 1.,
@@ -113,7 +113,7 @@ def test_single_paramset():
 
 
 def test_perform_gridsearch():
-    grid_settings = {'nthread': 2, 'grid_size': 2, 'num_classes': 10}
+    grid_settings = {'nthread': 28, 'grid_size': 2, 'num_classes': 10}
     cmssw_base_path = os.path.expandvars('$CMSSW_BASE')
     param_file = os.path.join(
         cmssw_base_path,
