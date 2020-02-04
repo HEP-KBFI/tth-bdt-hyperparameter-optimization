f90929574cd1a3861254b74d92a1af22609a86c8
Diana <diana.rand@outlook.com>
2020-02-04 11:06:06 +0200

    created script for testing GA with rosenbrock function

---
+++ scripts/rosenbrock_ga_q.py
@@ -0,0 +1,52 @@
+'''
+Genetic algorithm for finding the global minimum of the Rosenbrock function.
+Call with 'python'
+
+Usage: rosenbrock_ga_q.py
+'''
+import os
+import numpy as np
+from tthAnalysis.bdtHyperparameterOptimization import universal
+from tthAnalysis.bdtHyperparameterOptimization import ga_main as ga
+from tthAnalysis.bdtHyperparameterOptimization import rosenbrock_tools as rt
+
+np.random.seed(1)
+
+
+def main():
+    print('::::::: Reading settings and parameters :::::::')
+    cmssw_base_path = os.path.expandvars('$CMSSW_BASE')
+    main_dir = os.path.join(
+        cmssw_base_path,
+        'src',
+        'tthAnalysis',
+        'bdtHyperparameterOptimization'
+    )
+    settings_dir = os.path.join(
+        main_dir, 'data')
+    global_settings = universal.read_settings(settings_dir, 'global')
+    output_dir = os.path.expandvars(global_settings['output_dir'])
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    settings_dict = universal.read_settings(settings_dir, 'ga')
+    settings_dict.update(global_settings)
+    param_file = os.path.join(
+        settings_dir, 'rosenbrock_parameters.json')
+    param_dict = universal.read_parameters(param_file)
+    true_values = {'a': 1, 'b': 100}
+    result = ga.evolution(
+        settings_dict,
+        param_dict,
+        true_values,
+        rt.prepare_run_params,
+        rt.ensemble_fitness)
+    print(':::::::::: Saving results :::::::::::::')
+    rt.plot_progress(result, true_values, output_dir)
+    rt.plot_distance_history(result, true_values, output_dir)
+    rt.plot_fitness_history(result, output_dir)
+    rt.plot_2d_location_progress(result, true_values, output_dir)
+    rt.save_results(result, output_dir)
+
+
+if __name__ == '__main__':
+    main()
