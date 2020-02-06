26aa2e10f7471e71ff02da110a95ff23979347d2
Diana <diana.rand@outlook.com>
2020-02-06 15:08:29 +0200

    updates

--- python/gradient_tools.py
+++ python/gradient_tools.py
@@ -1,5 +1,14 @@
 '''Tools for a gradient descent algorithm for Rosenbrock function'''
+from sympy import symbols
 from tthAnalysis.bdtHyperparameterOptimization import rosenbrock_tools as rt
+
+
+def rosenbrock_diff(a, b):
+    x, y = symbols('x y')
+    z = (a - x) ** 2 + b * (y - x ** 2) ** 2
+    dx = z.diff(x)
+    dy = z.diff(y)
+    return dx, dy
 
 
 def update_values(curr_values, gradients, learning_rate):
@@ -9,10 +18,12 @@ def update_values(curr_values, gradients, learning_rate):
     return new_values
 
 
-def calculate_gradient(curr_values, a, b):
+def calculate_gradient(dx, dy, curr_values):
     gradients = {}
-    gradients['x'] = (-2 * (a - curr_values['x'])) - (4 * b * curr_values['x'] * (curr_values['y'] - (curr_values['x'] ** 2)))
-    gradients['y'] = 2 * b * (curr_values['y'] - (curr_values['x'] ** 2))
+    dx = dx.subs('x', curr_values['x'])
+    gradients['x'] = float(dx.subs('y', curr_values['y']))
+    dy = dy.subs('x', curr_values['x'])
+    gradients['y'] = float(dy.subs('y', curr_values['y']))
     return gradients
 
 
@@ -21,14 +32,17 @@ def gradient_descent(settings, parameters, true_values):
     iteration = 0
     # Choose random values
     value_set = rt.initialize_values(parameters)
+    # Calculate derivatives for the given function
+    dx, dy = rosenbrock_diff(true_values['a'], true_values['b'])
     while iteration <= settings['iterations'] or distance < 1e-8:
         print('Iteration: ' + str(iteration))
         curr_values = value_set
         # Calculate error
         # error = rt.ensemble_fitness([curr_values], true_values)
         # Calculate gradient
-        gradients = calculate_gradient(curr_values, true_values['a'], true_values['b'])
+        gradients = calculate_gradient(dx, dy, curr_values)
         # Adjust values with gradients
         value_set = update_values(curr_values, gradients, settings['learning_rate'])
         distance = rt.check_distance(true_values, value_set)
         iteration += 1
+    return value_set
