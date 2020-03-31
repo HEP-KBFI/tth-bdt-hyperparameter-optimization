140ef5f8ac02d9374fbdd85854fc02b3df029805
Laurits <laurits.tani@gmail.com>
2020-03-31 15:42:04 +0300

    updates

--- scripts/slurm_xgb_atlas.py
+++ scripts/slurm_xgb_atlas.py
@@ -34,7 +34,7 @@ def main(parameter_file, output_dir):
     data_dict = at.create_atlas_data_dict(path_to_file, global_settings)
     path = Path(parameter_file)
     saveDir = str(path.parent)
-    score, pred_train, pred_test, feature_importance, model = at.higgs_evaluation(
+    score, pred_train, pred_test, feature_importance = at.higgs_evaluation(
         parameter_dict, data_dict, nthread, num_classes)
     universal.save_info(
         score, pred_train, pred_test, saveDir, feature_importance)
