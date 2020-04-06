449b3e6f80230f9b21c503718589bbc15ecdc1cc
Laurits <laurits.tani@gmail.com>
2020-04-06 12:08:59 +0300

    testing

--- python/gradient_tools.py
+++ python/gradient_tools.py
@@ -248,6 +248,7 @@ def update_coordinates(point, true_values, step, evaluate):
             biggest_gradient_value_variable = None
             for variable in point.gradient:
                 biggest_gradient_value = 0
+                biggest_gradient_value_variable = variable
                 if abs(point.gradient[variable] > biggest_gradient_value):
                     biggest_gradient_value_variable = variable
                     biggest_gradient_value = point.gradient[variable]
